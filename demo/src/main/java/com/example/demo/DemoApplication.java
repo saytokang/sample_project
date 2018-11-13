691b5d6be824a854bdc05e94310e985bce0b87ae
kang don hee <saytokang@outlook.com>
2018-11-13 14:27:20 +0900

    DB 연결 작업

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.demo.dao.UserDao;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
 		System.out.println("\n\nHell World!!\n\n");
+		
+		UserDao userDao = ctx.getBean(UserDao.class);
+		userDao.getUserList().stream().forEach(System.out::println);
 	}
 }
